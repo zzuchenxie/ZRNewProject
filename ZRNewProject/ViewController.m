a2cc03ee9f457ff3feeb2a89f54339d35d68f0d8
zzuchenxie <zzuchenxie@gmail.com>
2014-05-25 18:53:21 +0800

    add a lbl

--- ZRNewProject/ViewController.m
+++ ZRNewProject/ViewController.m
@@ -19,6 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     NSLog(@"2014 0525 18:48");
+    UILabel  *LBL;
 }
 
 - (void)didReceiveMemoryWarning
